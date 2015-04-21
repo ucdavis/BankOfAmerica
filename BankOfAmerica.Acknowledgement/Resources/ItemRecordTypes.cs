234a4e38cad65aa44d9fb8b20e800464cafc5c20
John Knoll <jpknoll@ucdavis.edu>
2015-04-21 14:13:03 -0700

    dont forget dem resources

---
+++ BankOfAmerica.Acknowledgement/Resources/ItemRecordTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankOfAmerica.Acknowledgement.Resources
+{
+    public static class ItemRecordTypes
+    {
+        public static string FileHeaderRecord = "05";
+        public static string FileDetailRecord = "06";
+        public static string FileDetailAddendumRecord = "07";
+        public static string FileControlRecord = "09";
+    }
+}
---
+++ BankOfAmerica.Acknowledgement/Resources/ReceiptRecordTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankOfAmerica.Acknowledgement.Resources
+{
+    public static class ReceiptRecordTypes
+    {
+        public static string CashLetterHeaderRecord = "10";
+        public static string ItemDetailRecord = "25";
+        public static string ItemDetailAddendumRecord = "26";
+        public static string CashLetterControlRecord = "90";
+        public static string FileControlRecord = "99";
+    }
+}
