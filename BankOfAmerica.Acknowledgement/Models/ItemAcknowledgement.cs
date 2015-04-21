76d8fa6de9004bcd658f77435ba6c57ac7fe68ba
John Knoll <jpknoll@ucdavis.edu>
2015-04-21 15:57:33 -0700

    to/from string methods

--- BankOfAmerica.Acknowledgement/Models/ItemAcknowledgement.cs
+++ BankOfAmerica.Acknowledgement/Models/ItemAcknowledgement.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using BankOfAmerica.Acknowledgement.Models.Item;
+using BankOfAmerica.Acknowledgement.Resources;
 
 namespace BankOfAmerica.Acknowledgement.Models
 {
@@ -13,5 +17,52 @@ namespace BankOfAmerica.Acknowledgement.Models
         public FileHeaderRecord Header { get; set; }
         public List<FileDetailRecord> Details { get; set; }
         public FileControlRecord Control { get; set; }
+
+        public static ItemAcknowledgement FromString(StreamReader reader)
+        {
+            var result = new ItemAcknowledgement();
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var type = line.Substring(0, 2);
+
+                if (type == ItemRecordTypes.FileHeaderRecord)
+                {
+                    result.Header = FileHeaderRecord.FromString(line);
+                }
+                else if (type == ItemRecordTypes.FileDetailRecord)
+                {
+                    var detail = FileDetailRecord.FromString(line);
+                    result.Details.Add(detail);
+                }
+                else if (type == ItemRecordTypes.FileDetailAddendumRecord)
+                {
+                    var addendum = FileDetailAddendumRecord.FromString(line);
+                    // reference last added detail record
+                    result.Details.Last().Addendums.Add(addendum);
+                }
+                else if (type == ItemRecordTypes.FileControlRecord)
+                {
+                    result.Control = FileControlRecord.FromString(line);
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            ToString(builder);
+            return builder.ToString();
+        }
+
+        public void ToString(StringBuilder builder)
+        {
+            Header.ToString(builder);
+            Details.ForEach(d => d.ToString(builder));
+            Control.ToString(builder);
+        }
     }
 }
