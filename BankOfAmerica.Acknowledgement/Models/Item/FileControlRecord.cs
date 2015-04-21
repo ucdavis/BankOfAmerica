3c995ac341abaf58427fb8e5c9a0a4bce51bec47
John Knoll <jpknoll@ucdavis.edu>
2015-04-21 15:57:22 -0700

    fill out

--- BankOfAmerica.Acknowledgement/Models/Item/FileControlRecord.cs
+++ BankOfAmerica.Acknowledgement/Models/Item/FileControlRecord.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankOfAmerica.Acknowledgement.Resources;
 
 namespace BankOfAmerica.Acknowledgement.Models.Item
 {
+    /// <summary>
+    /// The File Control Record is the last record of the Acknowledgement 1/receipt acknowledgement data file and contains
+    /// seven fields. This record includes information from the input file that can be used to match the acknowledgement to
+    /// the original input file.
+    /// </summary>
     public class FileControlRecord
     {
+        /// <summary>
+        /// Value of field 2, Cash Letter Count, from the input File Control Record.
+        /// </summary>
+        public int TotalCashLetters { get; set; }
+
+        /// <summary>
+        /// Value of field 3, Total Record Count, from the input File Control Record.
+        /// </summary>
+        public int TotalRecordCount { get; set; }
+
+        /// <summary>
+        /// Value of field 4, Total Item Count, from the input File Control Record.
+        /// </summary>
+        public int TotalItemCount { get; set; }
+
+        /// <summary>
+        /// Value of field 5, File Total Amount, from the input File Control Record.
+        /// </summary>
+        public int FileTotalAmount { get; set; }
+
+        /// <summary>
+        /// Spaces. Reserved for future use
+        /// </summary>
+        public string Reserved { get; set; }
+
+        public static FileControlRecord FromString(string source)
+        {
+            // check record type
+            if (source.Substring(0, 2) != ItemRecordTypes.FileControlRecord)
+                throw new ArgumentException();
+
+            return new FileControlRecord()
+            {
+                TotalCashLetters = int.Parse(source.Substring(2, 6)),
+                TotalRecordCount = int.Parse(source.Substring(8, 8)),
+                TotalItemCount   = int.Parse(source.Substring(16, 8)),
+                FileTotalAmount  = int.Parse(source.Substring(24, 16)),
+                Reserved         = source.Substring(40, 40)
+            };
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            ToString(builder);
+            return builder.ToString();
+        }
+
+        public void ToString(StringBuilder builder)
+        {
+            builder.Append(ItemRecordTypes.FileControlRecord);
+            builder.Append(TotalCashLetters.ToString().PadLeft(6, '0'));
+            builder.Append(TotalRecordCount.ToString().PadLeft(8, '0'));
+            builder.Append(TotalItemCount.ToString().PadLeft(8, '0'));
+            builder.Append(FileTotalAmount.ToString().PadLeft(16, '0'));
+            builder.Append(Reserved.PadRight(40));
+            builder.AppendLine();
+        }
     }
 }
--- BankOfAmerica.Acknowledgement/Models/Item/FileDetailAddendumRecord.cs
+++ BankOfAmerica.Acknowledgement/Models/Item/FileDetailAddendumRecord.cs
@@ -1,12 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using BankOfAmerica.Acknowledgement.Resources;
 
 namespace BankOfAmerica.Acknowledgement.Models.Item
 {
+    /// <summary>
+    /// One or more Detail Addendum Records will follow each related Detail Record and contains five fields. This record will
+    /// include the details of the reason(s) a file was rejected, if applicable. If a file is rejected due to more than one type of
+    /// error, additional Detail Addendum Records will be included to report each additional error reason.
+    /// 
+    /// NOTE: If an error message contains too many characters to fit into one detail addendum record, additional detail
+    /// addendum records will be created until the entire error message is report
+    /// </summary>
     public class FileDetailAddendumRecord
     {
+        /// <summary>
+        /// Ordinal Number of Addendum Record
+        /// 
+        /// Note: The first addendum record following the
+        /// detail record will be '001', the second '002' and so
+        /// forth. The last ordinal should match the Addendum
+        /// Record Count of the Detail Record.
+        /// </summary>
+        public int AddendumRecordNumber { get; set; }
+
+        /// <summary>
+        /// Number of the current file error being reported.
+        /// Each file error will start on a new Detail Addendum
+        /// Record.
+        /// </summary>
+        public int FileRejectReasonNumber { get; set; }
+
+        /// <summary>
+        /// Reason file is being rejected if applicable, otherwise
+        /// spaces. File reject reasons may change from time
+        /// to time; sample error messages include:
+        /// 
+        /// The bundle item count exceeds the configured
+        ///     max
+        /// Business Date is a stale date
+        /// </summary>
+        public string FileRejectReason { get; set; }
+
+        public static FileDetailAddendumRecord FromString(string source)
+        {
+            // check record type
+            if (source.Substring(0, 2) != ItemRecordTypes.FileDetailAddendumRecord)
+                throw new ArgumentException();
+
+            return new FileDetailAddendumRecord()
+            {
+                AddendumRecordNumber   = int.Parse(source.Substring(2, 3)),
+                FileRejectReasonNumber = int.Parse(source.Substring(5, 2)),
+                FileRejectReason       = source.Substring(7, 73).Trim()
+            };
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            ToString(builder);
+            return builder.ToString();
+        }
+
+        public void ToString(StringBuilder builder)
+        {
+            builder.Append(ItemRecordTypes.FileDetailAddendumRecord);
+            builder.Append(AddendumRecordNumber.ToString().PadLeft(3, '0'));
+            builder.Append(FileRejectReasonNumber.ToString().PadLeft(2, '0'));
+            builder.Append(FileRejectReason.PadRight(73));
+            builder.AppendLine();
+        }
     }
 }
