0239622ed697ce85c750635602a0d08d86a61961
John Knoll <jpknoll@ucdavis.edu>
2015-04-21 16:12:06 -0700

    another test!

--- BankOfAmerica.Acknowledgement.Tests/ItemAcknowledgementTests.cs
+++ BankOfAmerica.Acknowledgement.Tests/ItemAcknowledgementTests.cs
@@ -8,9 +8,19 @@ namespace BankOfAmerica.Acknowledgement.Tests
     public class ItemAcknowledgementTests
     {
         [TestMethod]
-        public void TestFromString()
+        public void TestFromString1()
         {
             var fs = new StreamReader(@"..\..\data\TREGENTS_TREGENTSRecp150414125131818.ack");
+
+            var actual = ItemAcknowledgement.FromString(fs);
+
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void TestFromString2()
+        {
+            var fs = new StreamReader(@"..\..\data\TREGENTS_TREGENTSRecp150414125259411.ack");
 
             var actual = ItemAcknowledgement.FromString(fs);
 
