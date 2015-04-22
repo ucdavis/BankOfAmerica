77f6aa24935c8c877e3b5a6bf3ca5093c0724749
John Knoll <jpknoll@ucdavis.edu>
2015-04-22 08:54:42 -0700

    new test

---
+++ BankOfAmerica.Acknowledgement.Tests/ReceiptAcknowledgementTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using BankOfAmerica.Acknowledgement.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankOfAmerica.Acknowledgement.Tests
+{
+    [TestClass]
+    public class ReceiptAcknowledgementTests
+    {
+        [TestMethod]
+        public void TestFromString1()
+        {
+            var fs = new StreamReader(@"..\..\data\TREGENTS_TREGENTSAdjs20150414125414969.ack");
+
+            var actual = ReceiptAcknowledgement.FromString(fs);
+
+            Assert.IsNotNull(actual);
+        }
+    }
+}
